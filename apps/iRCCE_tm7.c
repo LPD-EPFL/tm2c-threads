3d5abdbf065986886d9a5ac58e8a3927530edb91
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-06 15:56:58 +0000

    using barriers
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@1983 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_tm7.c
+++ apps/iRCCE_tm7.c
@@ -110,11 +110,12 @@ MAIN(int argc, char **argv) {
         TX_COMMIT
     }
     
-    BARRIER
-
     PRINT("%02d\t%d\t%d\t%f", NUM_UES,
             stm_tx_node->tx_commited, (int) (stm_tx_node->tx_commited / duration__),  1000*(duration__ / stm_tx_node->tx_commited));
 
+    BARRIER
+
+    
     RCCE_shfree((t_vcharp) sis);
 
     TM_END
