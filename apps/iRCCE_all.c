2a11b12702bacb34fe3de8be46b01806b55592f7
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-06 17:43:15 +0000

    document and use assert
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@1992 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_all.c
+++ apps/iRCCE_all.c
@@ -9,36 +9,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "iRCCE.h"
 
 
+/*printing macros*/
 #define ME printf("[%02d] ", RCCE_ue())
 #define PRINTD(args...) {ME; printf(args); printf("\n"); fflush(stdout);}
 
-
+/*function headers*/
 void listen(int repeats);
 void send(int, int);
 inline long rand_range(long r);
 inline void srand_core();
 
-
 int rcounter = 0; /*receives counter  */
 int scounter = 0; /*sends counter*/
 int snccounter = 0; /*sends including the ones queued*/
 int print_success = 1; /*used to print the success message only once*/
-unsigned int number_sent = 0, number_sent_to = -1, to_send = 1;
+unsigned int number_sent = 0; /*the last integer that was send - used for validating the RESPONSE*/
+unsigned int number_sent_to = -1; /*the core to which the last REQUEST was sent*/
+unsigned int to_send = 1; /*if a new REQUEST should be sent (after the previous one is completed)*/
 iRCCE_WAIT_LIST waitlist, sendlist; /*incoming & outgoing msgs waitlists*/
 
+/*REQUEST or RESPONSE*/
 typedef enum {
     REQUEST,
     RESPONSE
 } TYPE;
 
 typedef struct {
-    TYPE type;
-    int number;
-    unsigned int from;
-    unsigned int to;
+    TYPE type; /*REQUEST or RESPONSE*/
+    int number; /*the integer that is sent*/
+    unsigned int from; /*the sender of the cmd*/
+    unsigned int to; /*the receiver of the cmd*/
 } cmd_t;
 
 int main(int argc, char *argv[]) {
@@ -48,28 +52,31 @@ int main(int argc, char *argv[]) {
 
     srand_core(); /*seed the rand()*/
 
-    int repeats; /*number of messages to send*/
+    int numreqs; /*number of REQUESTs to send*/
     if (argc == 1) {
-        repeats = 10000;
+        numreqs = 10000;
     }
     else {
-        repeats = atoi(argv[1]);
+        numreqs = atoi(argv[1]);
     }
 
-    listen(repeats);
+    listen(numreqs);
 
+    RCCE_finalize();
     return 0;
 }
 
-void listen(int repeats) {
+void listen(int numreqs) {
 
     int cores = RCCE_num_ues();
     int core = RCCE_ue();
-    iRCCE_RECV_REQUEST *recv_requests, *recv_current;
-    iRCCE_SEND_REQUEST *send_current;
-    char buf[cores * 32];
+    iRCCE_RECV_REQUEST *recv_requests; /*recvs for every other core*/
+    iRCCE_RECV_REQUEST *recv_current; /*to be used with the test function*/
+    iRCCE_SEND_REQUEST *send_current; /*to be used with the test funciton*/
+    char buf[cores * 32]; /*buffer for incoming data*/
     int sender;
 
+    /*init the waitlists*/
     iRCCE_init_wait_list(&waitlist);
     iRCCE_init_wait_list(&sendlist);
 
@@ -91,29 +98,21 @@ void listen(int repeats) {
     PRINTD("started..");
 
     while (1) {
-        /*if all sends are completed*/
-        if (scounter >= repeats && print_success) {
+        /*if all sends are completed print a success message once*/
+        if (scounter >= numreqs && print_success) {
             PRINTD("- %d -Sends completed! Have rcved %d msgs.", scounter, rcounter);
             print_success = 0;
         }
 
-        /*if not enough messages are sent yet & to_send*/
-        if (snccounter < repeats && to_send) {
+        /*if not enough messages have been sent yet & to_send*/
+        if (snccounter < numreqs && to_send) {
             send(-1, -1);
-            to_send = 0;
+            to_send = 0; /*wait for this request to be completed*/
         }
 
         /*test any send for completion*/
         iRCCE_test_any(&sendlist, &send_current, NULL);
         if (send_current != NULL) {
-            cmd_t * cmd = (cmd_t *) send_current->privbuf;
-
-/*
-            PRINTD("-->>[%s] to %02d (%d) for %d (%d)", cmd->type == REQUEST ? "REQUEST " : "RESPONSE", 
-                cmd->to, number_sent_to, cmd->number, number_sent);
-*/
-
-
             free(send_current->privbuf);
             free(send_current);
 
@@ -124,14 +123,13 @@ void listen(int repeats) {
         iRCCE_test_any(&waitlist, NULL, &recv_current);
         if (recv_current != NULL) {
 
-            /*the sender of the message*/
-            sender = recv_current->source;
+            sender = recv_current->source; /*the sender of the message*/
             char *base = buf + sender * 32;
-            /*the cmd sent*/
-            cmd_t * cmd = (cmd_t *) base;
+
+            cmd_t * cmd = (cmd_t *) base; /*retrieve the cmd received*/
 
             if (cmd->type == RESPONSE) {
-                /*if the wrong number received*/
+                /*if the wrong number received - validation failed*/
                 if (cmd->number != number_sent) {
                     PRINTD("[Expected] num: %5d from %02d, to %02d | [Received] num: %5d from %02d (%02d), to %02d",
                             number_sent, number_sent_to, RCCE_ue(), cmd->number, sender, cmd->from, cmd->to);
@@ -140,7 +138,7 @@ void listen(int repeats) {
                     to_send = 1; /*in order to send the next message*/
                 }
             }
-            else if (cmd->type == REQUEST) { /**/
+            else if (cmd->type == REQUEST) {
                 send(sender, cmd->number); /*simply send back the received number*/
             }
             else {
@@ -160,17 +158,15 @@ void send(int core, int number) {
 
     cmd_t cmd;
 
-    if (core < 0) {
+    if (core < 0) { /*new REQUEST*/
         do {
             core = rand_range(RCCE_num_ues()) - 1;
         } while (core == RCCE_ue());
-    }
 
-    if (number < 0) {
         cmd.type = REQUEST;
         cmd.number = ++number_sent;
         number_sent_to = core;
-    }
+    } /*RESPONSE*/
     else {
         cmd.type = RESPONSE;
         cmd.number = number;
@@ -178,10 +174,6 @@ void send(int core, int number) {
 
     cmd.from = RCCE_ue();
     cmd.to = core;
-
-    /*
-        PRINTD("[%s] to %02d for %d", cmd.type == REQUEST ? "REQUEST " : "RESPONSE", cmd.to, cmd.number);
-     */
 
     /*allocate the data buffer and the request that will be used for this send*/
     char *data = (char *) calloc(32, sizeof (char));
@@ -191,9 +183,12 @@ void send(int core, int number) {
         exit(-1);
     }
 
-    RC_cache_invalidate();
-    memcpy(data, &cmd, sizeof (cmd_t));
-
+    memcpy(data, &cmd, sizeof (cmd_t)); /*cp the data to the data buffer*/
+    cmd_t * cmd_vd = (cmd_t) data;
+    assert(cmd_vd->from == cmd.from);
+    assert(cmd_vd->number == cmd.number);
+    assert(cmd_vd->to == cmd.to);
+    assert(cmd_vd->type == cmd.type);
     if (iRCCE_isend(data, 32, core, s) != iRCCE_SUCCESS) {
         iRCCE_add_to_wait_list(&sendlist, s, NULL);
     }
