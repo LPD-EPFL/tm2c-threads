36a358377f21e050bfcce1bbe83f6a2a04b316cc
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-06 18:38:18 +0000

    no on the fly send
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2003 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_all.c
+++ apps/iRCCE_all.c
@@ -198,16 +198,19 @@ void send(int core, int number) {
     }
 
     memcpy(data, &cmd, sizeof (cmd_t)); /*cp the data to the data buffer*/
+    iRCCE_isend(data, 32, core, s);
+/*
     if (iRCCE_isend(data, 32, core, s) != iRCCE_SUCCESS) {
         iRCCE_add_to_wait_list(&sendlist, s, NULL);
     }
-    else { /*message delivered*/
+    else { /*message delivered/
         PRINTD("On the fly: [%s] num: %5d from %02d, to %02d ", 
                 cmd.type == REQUEST ? "REQUEST" : "RESPONSE", cmd.number, cmd.from, cmd.to);
         free(s);
         free(data);
         scounter++;
     }
+*/
 
     /*increase the "non-confirmed" counter*/
     snccounter++;
