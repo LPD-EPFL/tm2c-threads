3ccf19b1619bb650f0159a005c99e47b28e0fb67
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-06 17:46:31 +0000

    cmd_t * <<
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@1994 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_all.c
+++ apps/iRCCE_all.c
@@ -185,14 +185,18 @@ void send(int core, int number) {
 
     memcpy(data, &cmd, sizeof (cmd_t)); /*cp the data to the data buffer*/
     cmd_t * cmd_vd = (cmd_t *) data;
-    assert(cmd_vd->from == cmd.from);
-    assert(cmd_vd->number == cmd.number);
-    assert(cmd_vd->to == cmd.to);
-    assert(cmd_vd->type == cmd.type);
     if (iRCCE_isend(data, 32, core, s) != iRCCE_SUCCESS) {
         iRCCE_add_to_wait_list(&sendlist, s, NULL);
+        assert(cmd_vd->from == cmd.from);
+        assert(cmd_vd->number == cmd.number);
+        assert(cmd_vd->to == cmd.to);
+        assert(cmd_vd->type == cmd.type);
     }
     else {
+        assert(cmd_vd->from == cmd.from);
+        assert(cmd_vd->number == cmd.number);
+        assert(cmd_vd->to == cmd.to);
+        assert(cmd_vd->type == cmd.type);
         free(s);
         free(data);
         scounter++;
