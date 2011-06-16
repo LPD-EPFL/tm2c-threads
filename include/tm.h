376e8d2a1de1fafb303978c595cb9488a878b88e
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-16 10:40:12 +0000

    tm.c
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2467 dd814760-9b98-ad33-1d6a-b699670a4318

--- include/tm.h
+++ include/tm.h
@@ -27,18 +27,13 @@ extern "C" {
 
 
 #define BACKOFF
-#define BACKOFF_MAX 3
-#define BACKOFF_DELAY 400
+#define BACKOFF_MAX                     3
+#define BACKOFF_DELAY                   400
 
     extern stm_tx_t *stm_tx;
     extern stm_tx_node_t *stm_tx_node;
 
-//    const char *conflict_reasons[4] = {
-//        "NO_CONFLICT",
-//        "READ_AFTER_WRITE",
-//        "WRITE_AFTER_READ",
-//        "WRITE_AFTER_WRITE"
-//    };
+    extern const char *conflict_reasons[4];
 
     /*______________________________________________________________________________________________________
      * TM Interface                                                                                         |
@@ -64,26 +59,6 @@ extern "C" {
         unsigned int ID = RCCE_ue();                                    \
         unsigned int NUM_UES = RCCE_num_ues();                          \
         tm_init(ID);
-
-    inline int color(int id, void *aux) {
-        return !(id % DSLNDPERNODES);
-    }
-
-    inline void tm_init(unsigned int ID) {
-        if (ID % DSLNDPERNODES == 0) {
-            //dsl node
-            dsl_init();
-        }
-        else { //app node
-            ps_init_();
-            stm_tx_node = tx_metadata_node_new();
-            stm_tx = tx_metadata_new(IDLE);
-            if (stm_tx == NULL || stm_tx_node == NULL) {
-                PRINTD("Could not alloc tx metadata @ TM_INIT");
-                EXIT(-1);
-            }
-        }
-    }
 
 #define TX_START                                                        \
     { PRINTD("|| Starting new tx");                                     \
@@ -112,84 +87,56 @@ extern "C" {
         ? stm_tx->max_retries : stm_tx->retries;                        
 
 
-#define TX_ABORT(reason)                                \
-    PRINTD("|| aborting tx");                           \
-    handle_abort(stm_tx, reason);                       \
+#define TX_ABORT(reason)                                                \
+    PRINTD("|| aborting tx");                                           \
+    handle_abort(stm_tx, reason);                                       \
     siglongjmp(stm_tx->env, reason);
 
-    //ps_hashtable_print(ps_hashtable);                   \
-    
-#define TX_COMMIT                                       \
-    PRINTD("|| commiting tx");                          \
-    ps_publish_all();                                   \
-    write_set_persist(stm_tx->write_set);               \
-    ps_finish_all();                                    \
-    stm_tx->state = COMMITED;                           \
-    mem_info_on_commit(stm_tx->mem_info);               \
-    stm_tx_node->tx_starts += stm_tx->retries;          \
-    stm_tx_node->tx_commited++;                         \
-    stm_tx_node->tx_aborted += stm_tx->aborts;          \
-    stm_tx_node->max_retries =                          \
-        (stm_tx->max_retries < stm_tx_node->max_retries)\
-            ? stm_tx_node->max_retries                  \
-            : stm_tx->max_retries;                      \
-    stm_tx_node->aborts_war += stm_tx->aborts_war;      \
-    stm_tx_node->aborts_raw += stm_tx->aborts_raw;      \
-    stm_tx_node->aborts_waw += stm_tx->aborts_waw;      \
+#define TX_COMMIT                                                       \
+    PRINTD("|| commiting tx");                                          \
+    ps_publish_all();                                                   \
+    write_set_persist(stm_tx->write_set);                               \
+    ps_finish_all();                                                    \
+    stm_tx->state = COMMITED;                                           \
+    mem_info_on_commit(stm_tx->mem_info);                               \
+    stm_tx_node->tx_starts += stm_tx->retries;                          \
+    stm_tx_node->tx_commited++;                                         \
+    stm_tx_node->tx_aborted += stm_tx->aborts;                          \
+    stm_tx_node->max_retries =                                          \
+        (stm_tx->max_retries < stm_tx_node->max_retries)                \
+            ? stm_tx_node->max_retries                                  \
+            : stm_tx->max_retries;                                      \
+    stm_tx_node->aborts_war += stm_tx->aborts_war;                      \
+    stm_tx_node->aborts_raw += stm_tx->aborts_raw;                      \
+    stm_tx_node->aborts_waw += stm_tx->aborts_waw;                      \
     stm_tx = tx_metadata_empty(stm_tx); }
 
 
-#define TM_END                                          \
-    PRINTD("|| FAKE: TM ends");                         \
+#define TM_END                                                          \
+    PRINTD("|| FAKE: TM ends");                                         \
+    tx_metadata_free(&stm_tx);                                          \
+    free(stm_tx_node); }
+
+#define TM_END_STATS                                                    \
+    PRINTD("|| FAKE: TM ends");                                         \
+    tx_metadata_free(&stm_tx);                                          \
+    tx_metadata_node_print(stm_tx_node);                                \
     free(stm_tx_node); }                                  
 
-#define TM_END_STATS                                    \
-    PRINTD("|| FAKE: TM ends");                         \
-    tx_metadata_node_print(stm_tx_node);                \
-    free(stm_tx_node); }                                  
 
-
-#define TX_LOAD(addr)                                   \
+#define TX_LOAD(addr)                                                   \
     tx_load(stm_tx->write_set, stm_tx->read_set, ((void *) (addr)))
 
-#define TX_STORE(addr, ptr, datatype)                   \
+#define TX_STORE(addr, ptr, datatype)                                   \
     write_set_update(stm_tx->write_set, datatype, ((void *) (ptr)), ((void *) (addr)))
-    
+
     /*early release of READ lock -- TODO: the entry remains in read-set, so one
      SHOULD NOT try to re-read the address cause the tx things it keeps the lock*/
-#define TX_RRLS(addr)                                   \
+#define TX_RRLS(addr)                                                   \
     ps_unsubscribe((void*) (addr));
 
-#define TX_WRLS(addr)                                   \
+#define TX_WRLS(addr)                                                   \
     ps_publish_finish((void*) (addr));
-
-#define taskudelay udelay
-    inline void udelay(unsigned int micros) {
-        double __ts_end = RCCE_wtime() + ((double) micros / 1000000);
-        while (RCCE_wtime() < __ts_end);
-    }
-
-    inline void ps_unsubscribe_all();
-
-    inline void handle_abort(stm_tx_t *stm_tx, CONFLICT_TYPE reason) {
-        ps_finish_all();
-        stm_tx->state = ABORTED;
-        stm_tx->aborts++;
-        switch (reason) {
-            case READ_AFTER_WRITE:
-                stm_tx->aborts_raw++;
-                break;
-            case WRITE_AFTER_READ:
-                stm_tx->aborts_war++;
-                break;
-            case WRITE_AFTER_WRITE:
-                stm_tx->aborts_waw++;
-        }
-        //PRINTD("  | read/write_set_free");
-        write_set_empty(stm_tx->write_set);
-        read_set_empty(stm_tx->read_set);
-        mem_info_on_abort(stm_tx->mem_info);
-    }
 
     /*__________________________________________________________________________________________
      * TRANSACTIONAL MEMORY ALLOCATION
@@ -208,78 +155,29 @@ extern "C" {
 #define TX_SHFREE(addr)                                                   \
     stm_shfree(stm_tx->mem_info, (t_vcharp) addr)
 
-    inline void * tx_load(write_set_t *ws, read_set_t *rs, void *addr) {
-        write_entry_t *we;
-        if ((we = write_set_contains(ws, addr)) != NULL) {
-            read_set_update(rs, addr);
-            return (void *) &we->i;
-        }
-        else {
-            if (!read_set_update(rs, addr)) {
-                //the node is NOT already subscribed for the address
-                CONFLICT_TYPE conflict;
-#ifdef BACKOFF
-                unsigned int num_delays = 0;
-                unsigned int delay = BACKOFF_DELAY;
+    
+#define taskudelay udelay
 
-retry:
-#endif
-                if ((conflict = ps_subscribe((void *) addr)) != NO_CONFLICT) {
-#ifdef BACKOFF
-                    if (num_delays++ < BACKOFF_MAX) {
-                        udelay(delay);
-                        delay *= 2;
-                        goto retry;
-                    }
-#endif
-                    TX_ABORT(conflict);
-                }
-            }
-            return addr;
-        }
+    inline void udelay(unsigned int micros) {
+        double __ts_end = RCCE_wtime() + ((double) micros / 1000000);
+        while (RCCE_wtime() < __ts_end);
     }
 
-    inline void ps_publish_finish_all(unsigned int locked) {
-        locked = (locked != 0) ? locked : stm_tx->write_set->nb_entries;
-        write_entry_t *we_current = stm_tx->write_set->write_entries;
-        while (locked-- > 0) {
-            ps_publish_finish((void *) we_current[locked].address_shmem);
-        }
-    }
+    inline void ps_unsubscribe_all();
 
-    inline void ps_publish_all() {
-        unsigned int locked = 0;
-        write_entry_t *write_entries = stm_tx->write_set->write_entries;
-        unsigned int nb_entries = stm_tx->write_set->nb_entries;
-        while (locked < nb_entries) {
-            CONFLICT_TYPE conflict;
-#ifdef BACKOFF
-            unsigned int num_delays = 0;
-            unsigned int delay = BACKOFF_DELAY; //micro
-retry:
-#endif
-            if ((conflict = ps_publish((void *) write_entries[locked].address_shmem)) != NO_CONFLICT) {
-                //ps_publish_finish_all(locked);
-#ifdef BACKOFF
-                if (num_delays++ < BACKOFF_MAX) {
-                    udelay(delay);
-                    delay *= 2;
-                    goto retry;
-                }
-#endif
-                TX_ABORT(conflict);
-            }
-            locked++;
-        }
-    }
+    inline void handle_abort(stm_tx_t *stm_tx, CONFLICT_TYPE reason);
 
-    inline void ps_unsubscribe_all() {
-        read_entry_l_t *read_entries = stm_tx->read_set->read_entries;
-        int i;
-        for (i = 0; i < stm_tx->read_set->nb_entries; i++) {
-            ps_unsubscribe((void *) read_entries[i].address_shmem);
-        }
-    }
+    int color(int id, void *aux);
+
+    void tm_init(unsigned int ID);
+
+    inline void * tx_load(write_set_t *ws, read_set_t *rs, void *addr);
+
+    inline void ps_publish_finish_all(unsigned int locked);
+
+    inline void ps_publish_all();
+
+    inline void ps_unsubscribe_all();
 
 #ifdef	__cplusplus
 }
---
+++ src/tm.c
@@ -0,0 +1,136 @@
+/* 
+ * File:   tm.c
+ * Author: trigonak
+ *
+ * Created on June 16, 2011, 12:29 PM
+ */
+
+#include "tm.h"
+
+extern stm_tx_t *stm_tx = NULL;
+extern stm_tx_node_t *stm_tx_node = NULL;
+
+extern const char *conflict_reasons[4] = {
+    "NO_CONFLICT",
+    "READ_AFTER_WRITE",
+    "WRITE_AFTER_READ",
+    "WRITE_AFTER_WRITE"
+};
+
+/*______________________________________________________________________________________________________
+ * TM Interface                                                                                         |
+ *______________________________________________________________________________________________________|
+ */
+
+int color(int id, void *aux) {
+    return !(id % DSLNDPERNODES);
+}
+
+void tm_init(unsigned int ID) {
+    if (ID % DSLNDPERNODES == 0) {
+        //dsl node
+        dsl_init();
+    }
+    else { //app node
+        ps_init_();
+        stm_tx_node = tx_metadata_node_new();
+        stm_tx = tx_metadata_new(IDLE);
+        if (stm_tx == NULL || stm_tx_node == NULL) {
+            PRINTD("Could not alloc tx metadata @ TM_INIT");
+            EXIT(-1);
+        }
+    }
+}
+
+inline void handle_abort(stm_tx_t *stm_tx, CONFLICT_TYPE reason) {
+    ps_finish_all();
+    stm_tx->state = ABORTED;
+    stm_tx->aborts++;
+    switch (reason) {
+        case READ_AFTER_WRITE:
+            stm_tx->aborts_raw++;
+            break;
+        case WRITE_AFTER_READ:
+            stm_tx->aborts_war++;
+            break;
+        case WRITE_AFTER_WRITE:
+            stm_tx->aborts_waw++;
+    }
+    //PRINTD("  | read/write_set_free");
+    write_set_empty(stm_tx->write_set);
+    read_set_empty(stm_tx->read_set);
+    mem_info_on_abort(stm_tx->mem_info);
+}
+
+inline void * tx_load(write_set_t *ws, read_set_t *rs, void *addr) {
+    write_entry_t *we;
+    if ((we = write_set_contains(ws, addr)) != NULL) {
+        read_set_update(rs, addr);
+        return (void *) &we->i;
+    }
+    else {
+        if (!read_set_update(rs, addr)) {
+            //the node is NOT already subscribed for the address
+            CONFLICT_TYPE conflict;
+#ifdef BACKOFF
+            unsigned int num_delays = 0;
+            unsigned int delay = BACKOFF_DELAY;
+
+retry:
+#endif
+            if ((conflict = ps_subscribe((void *) addr)) != NO_CONFLICT) {
+#ifdef BACKOFF
+                if (num_delays++ < BACKOFF_MAX) {
+                    udelay(delay);
+                    delay *= 2;
+                    goto retry;
+                }
+#endif
+                TX_ABORT(conflict);
+            }
+        }
+        return addr;
+    }
+}
+
+inline void ps_publish_finish_all(unsigned int locked) {
+    locked = (locked != 0) ? locked : stm_tx->write_set->nb_entries;
+    write_entry_t *we_current = stm_tx->write_set->write_entries;
+    while (locked-- > 0) {
+        ps_publish_finish((void *) we_current[locked].address_shmem);
+    }
+}
+
+inline void ps_publish_all() {
+    unsigned int locked = 0;
+    write_entry_t *write_entries = stm_tx->write_set->write_entries;
+    unsigned int nb_entries = stm_tx->write_set->nb_entries;
+    while (locked < nb_entries) {
+        CONFLICT_TYPE conflict;
+#ifdef BACKOFF
+        unsigned int num_delays = 0;
+        unsigned int delay = BACKOFF_DELAY; //micro
+retry:
+#endif
+        if ((conflict = ps_publish((void *) write_entries[locked].address_shmem)) != NO_CONFLICT) {
+            //ps_publish_finish_all(locked);
+#ifdef BACKOFF
+            if (num_delays++ < BACKOFF_MAX) {
+                udelay(delay);
+                delay *= 2;
+                goto retry;
+            }
+#endif
+            TX_ABORT(conflict);
+        }
+        locked++;
+    }
+}
+
+inline void ps_unsubscribe_all() {
+    read_entry_l_t *read_entries = stm_tx->read_set->read_entries;
+    int i;
+    for (i = 0; i < stm_tx->read_set->nb_entries; i++) {
+        ps_unsubscribe((void *) read_entries[i].address_shmem);
+    }
+}
