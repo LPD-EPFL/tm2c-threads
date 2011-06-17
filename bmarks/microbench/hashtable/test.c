dd95263166645092ef8ec7ab2953f9338601074b
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-17 16:45:38 +0000

    removed nb_aborts field
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2533 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/hashtable/test.c
+++ bmarks/microbench/hashtable/test.c
@@ -523,7 +523,6 @@ int main(int argc, char **argv) {
     data->nb_snapshoted = 0;
     data->nb_contains = 0;
     data->nb_found = 0;
-    data->nb_aborts = 0;
     data->set = set;
 
     printf("---------------------------Thread %d\n", RCCE_ue());
