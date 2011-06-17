c2b7a8e93600fd436b3d4ad0ab7a85029178e4d8
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-17 16:13:36 +0000

    microbenchmark/hashtable
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2532 dd814760-9b98-ad33-1d6a-b699670a4318

---
+++ bmarks/microbench/hashtable/test.c
@@ -0,0 +1,549 @@
+/*
+ * File:
+ *   test.c
+ * Author(s):
+ *   Vincent Gramoli <vincent.gramoli@epfl.ch>
+ * Description:
+ *   Concurrent accesses of a hashtable
+ *
+ * Copyright (c) 2009-2010.
+ *
+ * test.c is part of Microbench
+ * 
+ * Microbench is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "intset.h"
+
+/* Hashtable length (# of buckets) */
+unsigned int maxhtlength;
+
+/* ################################################################### *
+ * RANDOM
+ * ################################################################### */
+
+/* 
+ * Returns a pseudo-random value in [1;range).
+ * Depending on the symbolic constant RAND_MAX>=32767 defined in stdlib.h,
+ * the granularity of rand() could be lower-bounded by the 32767^th which might 
+ * be too high for given values of range and initial.
+ */
+inline long rand_range(long r) {
+    int m = RAND_MAX;
+    long d, v = 0;
+
+    do {
+        d = (m > r ? r : m);
+        v += 1 + (long) (d * ((double) rand() / ((double) (m) + 1.0)));
+        r -= m;
+    } while (r > 0);
+    return v;
+}
+
+/*
+ * Seeding the rand()
+ */
+inline void srand_core() {
+    double timed_ = RCCE_wtime();
+    unsigned int timeprfx_ = (unsigned int) timed_;
+    unsigned int time_ = (unsigned int) ((timed_ - timeprfx_) * 1000000);
+    srand(time_ + (13 * (RCCE_ue() + 1)));
+}
+
+typedef struct thread_data {
+    val_t first;
+    long range;
+    int update;
+    int move;
+    int snapshot;
+    int unit_tx;
+    int alternate;
+    int effective;
+    unsigned long nb_add;
+    unsigned long nb_added;
+    unsigned long nb_remove;
+    unsigned long nb_removed;
+    unsigned long nb_contains;
+    /* added for HashTables */
+    unsigned long load_factor;
+    unsigned long nb_move;
+    unsigned long nb_moved;
+    unsigned long nb_snapshot;
+    unsigned long nb_snapshoted;
+    /* end: added for HashTables */
+    unsigned long nb_found;
+    ht_intset_t *set;
+} thread_data_t;
+
+void *test(void *data) {
+    int val2, numtx, r, last = -1;
+    val_t val = 0;
+    int unext, mnext, cnext;
+
+    thread_data_t *d = (thread_data_t *) data;
+
+    srand_core();
+
+    TM_INITs
+
+    /* Is the first op an update, a move? */
+    r = rand_range(100) - 1;
+    unext = (r < d->update);
+    mnext = (r < d->move);
+    cnext = (r >= d->update + d->snapshot);
+
+    FOR(10) {
+
+        if (unext) { // update
+
+            if (mnext) { // move
+
+                if (last == -1) val = rand_range(d->range);
+                else val = last;
+                val2 = rand_range(d->range);
+                if (ht_move(d->set, val, val2, TRANSACTIONAL)) {
+                    d->nb_moved++;
+                    last = -1;
+                }
+                d->nb_move++;
+
+            }
+            else if (last < 0) { // add
+
+                val = rand_range(d->range);
+                if (ht_add(d->set, val, TRANSACTIONAL)) {
+                    d->nb_added++;
+                    last = val;
+                }
+                d->nb_add++;
+
+            }
+            else { // remove
+
+                if (d->alternate) { // alternate mode
+                    if (ht_remove(d->set, last, TRANSACTIONAL)) {
+                        d->nb_removed++;
+                        last = -1;
+                    }
+                }
+                else {
+                    /* Random computation only in non-alternated cases */
+                    val = rand_range(d->range);
+                    /* Remove one random value */
+                    if (ht_remove(d->set, val, TRANSACTIONAL)) {
+                        d->nb_removed++;
+                        /* Repeat until successful, to avoid size variations */
+                        last = -1;
+                    }
+                }
+                d->nb_remove++;
+            }
+
+        }
+        else { // reads
+
+            if (cnext) { // contains (no snapshot)
+
+                if (d->alternate) {
+                    if (d->update == 0) {
+                        if (last < 0) {
+                            val = d->first;
+                            last = val;
+                        }
+                        else { // last >= 0
+                            val = rand_range(d->range);
+                            last = -1;
+                        }
+                    }
+                    else { // update != 0
+                        if (last < 0) {
+                            val = rand_range(d->range);
+                            //last = val;
+                        }
+                        else {
+                            val = last;
+                        }
+                    }
+                }
+                else val = rand_range(d->range);
+
+                if (ht_contains(d->set, val, TRANSACTIONAL))
+                    d->nb_found++;
+                d->nb_contains++;
+
+            }
+            else { // snapshot
+
+                if (ht_snapshot(d->set, TRANSACTIONAL))
+                    d->nb_snapshoted++;
+                d->nb_snapshot++;
+
+            }
+        }
+
+        /* Is the next op an update, a move, a contains? */
+        if (d->effective) { // a failed remove/add is a read-only tx
+            numtx = d->nb_contains + d->nb_add + d->nb_remove + d->nb_move + d->nb_snapshot;
+            unext = ((100.0 * (d->nb_added + d->nb_removed + d->nb_moved)) < (d->update * numtx));
+            mnext = ((100.0 * d->nb_moved) < (d->move * numtx));
+            cnext = !((100.0 * d->nb_snapshoted) < (d->snapshot * numtx));
+        }
+        else { // remove/add (even failed) is considered as an update
+            r = rand_range(100) - 1;
+            unext = (r < d->update);
+            mnext = (r < d->move);
+            cnext = (r >= d->update + d->snapshot);
+        }
+
+    }
+
+    TM_END_STATS
+
+            /* Free transaction */
+
+    return NULL;
+}
+
+void *test2(void *data) {
+    int val, newval, last, flag = 1;
+    thread_data_t *d = (thread_data_t *) data;
+
+    srand_core();
+
+    /* Create transaction */
+    TM_INITs
+    /* Wait on barrier */
+
+    last = 0; // to avoid warning
+
+    FOR(10) {
+        val = rand_range(100) - 1;
+        /* added for HashTables */
+        if (val < d->update) {
+            if (val >= d->move) { /* update without move */
+                if (flag) {
+                    /* Add random value */
+                    val = (rand_range(d->range)) + 1;
+                    if (ht_add(d->set, val, TRANSACTIONAL)) {
+                        d->nb_added++;
+                        last = val;
+                        flag = 0;
+                    }
+                    d->nb_add++;
+                }
+                else {
+                    if (d->alternate) {
+                        /* Remove last value */
+                        if (ht_remove(d->set, last, TRANSACTIONAL))
+                            d->nb_removed++;
+                        d->nb_remove++;
+                        flag = 1;
+                    }
+                    else {
+                        /* Random computation only in non-alternated cases */
+                        newval = rand_range(d->range);
+                        if (ht_remove(d->set, newval, TRANSACTIONAL)) {
+                            d->nb_removed++;
+                            /* Repeat until successful, to avoid size variations */
+                            flag = 1;
+                        }
+                        d->nb_remove++;
+                    }
+                }
+            }
+            else { /* move */
+                val = rand_range(d->range);
+                if (ht_move(d->set, last, val, TRANSACTIONAL)) {
+                    d->nb_moved++;
+                    last = val;
+                }
+                d->nb_move++;
+            }
+        }
+        else {
+            if (val >= d->update + d->snapshot) { /* read-only without snapshot */
+                /* Look for random value */
+                val = rand_range(d->range);
+                if (ht_contains(d->set, val, TRANSACTIONAL))
+                    d->nb_found++;
+                d->nb_contains++;
+            }
+            else { /* snapshot */
+                if (ht_snapshot(d->set, TRANSACTIONAL))
+                    d->nb_snapshoted++;
+                d->nb_snapshot++;
+            }
+        }
+    }
+
+    /* Free transaction */
+    TM_END
+    return NULL;
+}
+
+void print_set(intset_t *set) {
+    node_t *curr, *tmp;
+
+    curr = set->head;
+    tmp = curr;
+    do {
+        printf(" - v%d", (int) curr->val);
+        tmp = curr;
+        curr = tmp->next;
+    } while (curr->val != VAL_MAX);
+    printf(" - v%d", (int) curr->val);
+    printf("\n");
+    FLUSH;
+}
+
+void print_ht(ht_intset_t *set) {
+    int i;
+    for (i = 0; i < maxhtlength; i++) {
+        print_set(set->buckets[i]);
+    }
+}
+
+int main(int argc, char **argv) {
+    struct option long_options[] = {
+        // These options don't set a flag
+        {"help", no_argument, NULL, 'h'},
+        {"duration", required_argument, NULL, 'd'},
+        {"initial-size", required_argument, NULL, 'i'},
+        {"range", required_argument, NULL, 'r'},
+        {"update-rate", required_argument, NULL, 'u'},
+        {"move-rate", required_argument, NULL, 'm'},
+        {"snapshot-rate", required_argument, NULL, 'a'},
+        {"elasticity", required_argument, NULL, 'x'},
+        {NULL, 0, NULL, 0}
+    };
+
+    ht_intset_t *set;
+    int i, c, size;
+    val_t last = 0;
+    val_t val = 0;
+    thread_data_t *data;
+    struct timeval start, end;
+    struct timespec timeout;
+    int duration = DEFAULT_DURATION;
+    int initial = DEFAULT_INITIAL;
+#ifdef DSL
+    int nb_app_cores = (RCCE_num_ues() / 2) + ((RCCE_num_ues() % 2) ? 1 : 0);
+#else
+    int nb_app_cores = RCCE_num_ues();
+#endif
+    long range = DEFAULT_RANGE;
+    int update = DEFAULT_UPDATE;
+    int load_factor = DEFAULT_LOAD;
+    int move = DEFAULT_MOVE;
+    int snapshot = DEFAULT_SNAPSHOT;
+    int unit_tx = DEFAULT_ELASTICITY;
+    int alternate = DEFAULT_ALTERNATE;
+    int effective = DEFAULT_EFFECTIVE;
+    sigset_t block_set;
+
+    while (1) {
+        i = 0;
+        c = getopt_long(argc, argv, "hAf:d:i:n:r:s:u:m:a:l:x:", long_options, &i);
+
+        if (c == -1)
+            break;
+
+        if (c == 0 && long_options[i].flag == 0)
+            c = long_options[i].val;
+
+        switch (c) {
+            case 0:
+                // Flag is automatically set 
+                break;
+            case 'h':
+                ONCE
+            {
+                printf("intset -- STM stress test "
+                        "(hash table)\n"
+                        "\n"
+                        "Usage:\n"
+                        "  intset [options...]\n"
+                        "\n"
+                        "Options:\n"
+                        "  -h, --help\n"
+                        "        Print this message\n"
+                        "  -A, --Alternate\n"
+                        "        Consecutive insert/remove target the same value\n"
+                        "  -f, --effective <int>\n"
+                        "        update txs must effectively write (0=trial, 1=effective, default=" XSTR(DEFAULT_EFFECTIVE) ")\n"
+                        "  -d, --duration <int>\n"
+                        "        Test duration in milliseconds (0=infinite, default=" XSTR(DEFAULT_DURATION) ")\n"
+                        "  -i, --initial-size <int>\n"
+                        "        Number of elements to insert before test (default=" XSTR(DEFAULT_INITIAL) ")\n"
+                        "  -n, --num-threads <int>\n"
+                        "        Number of threads (default=" XSTR(DEFAULT_NB_THREADS) ")\n"
+                        "  -r, --range <int>\n"
+                        "        Range of integer values inserted in set (default=" XSTR(DEFAULT_RANGE) ")\n"
+                        "  -s, --seed <int>\n"
+                        "        RNG seed (0=time-based, default=" XSTR(DEFAULT_SEED) ")\n"
+                        "  -u, --update-rate <int>\n"
+                        "        Percentage of update transactions (default=" XSTR(DEFAULT_UPDATE) ")\n"
+                        "  -m , --move-rate <int>\n"
+                        "        Percentage of move transactions (default=" XSTR(DEFAULT_MOVE) ")\n"
+                        "  -a , --snapshot-rate <int>\n"
+                        "        Percentage of snapshot transactions (default=" XSTR(DEFAULT_SNAPSHOT) ")\n"
+                        "  -l , --load-factor <int>\n"
+                        "        Ratio of keys over buckets (default=" XSTR(DEFAULT_LOAD) ")\n"
+                        "  -x, --elasticity (default=4)\n"
+                        "        Use elastic transactions\n"
+                        "        0 = non-protected,\n"
+                        "        1 = normal transaction,\n"
+                        "        2 = read elastic-tx,\n"
+                        "        3 = read/add elastic-tx,\n"
+                        "        4 = read/add/rem elastic-tx,\n"
+                        "        5 = elastic-tx w/ optimized move.\n"
+                        );
+            }
+                exit(0);
+            case 'A':
+                alternate = 1;
+                break;
+            case 'f':
+                effective = atoi(optarg);
+                break;
+            case 'd':
+                duration = atoi(optarg);
+                break;
+            case 'i':
+                initial = atoi(optarg);
+                break;
+            case 'n':
+                nb_app_cores = atoi(optarg);
+                break;
+            case 'r':
+                range = atol(optarg);
+                break;
+            case 'u':
+                update = atoi(optarg);
+                break;
+            case 'm':
+                move = atoi(optarg);
+                break;
+            case 'a':
+                snapshot = atoi(optarg);
+                break;
+            case 'l':
+                load_factor = atoi(optarg);
+                break;
+            case 'x':
+                unit_tx = atoi(optarg);
+                break;
+            case '?':
+                ONCE
+            {
+                printf("Use -h or --help for help\n");
+            }
+                exit(0);
+            default:
+                exit(1);
+        }
+    }
+
+    assert(duration >= 0);
+    assert(initial >= 0);
+    assert(nb_app_cores > 0);
+    assert(range > 0 && range >= initial);
+    assert(update >= 0 && update <= 100);
+    assert(move >= 0 && move <= update);
+    assert(snapshot >= 0 && snapshot <= (100 - update));
+    assert(initial < MAXHTLENGTH);
+    assert(initial >= load_factor);
+
+    ONCE
+    {
+        printf("Set type     : hash table\n");
+        printf("Duration     : %d\n", duration);
+        printf("Initial size : %d\n", initial);
+        printf("Nb threads   : %d\n", nb_app_cores);
+        printf("Value range  : %ld\n", range);
+        printf("Update rate  : %d\n", update);
+        printf("Load factor  : %d\n", load_factor);
+        printf("Move rate    : %d\n", move);
+        printf("Snapshot rate: %d\n", snapshot);
+        printf("Elasticity   : %d\n", unit_tx);
+        printf("Alternate    : %d\n", alternate);
+        printf("Effective    : %d\n", effective);
+        FLUSH;
+    }
+
+    if ((data = (thread_data_t *) malloc(sizeof (thread_data_t))) == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    maxhtlength = (unsigned int) initial / load_factor;
+
+
+    set = ht_new();
+    // Populate set 
+    printf("Adding %d entries to set\n", initial);
+    i = 0;
+    maxhtlength = (int) (initial / load_factor);
+    while (i < initial) {
+        val = rand_range(range);
+        if (ht_add(set, val, 0)) {
+            last = val;
+            i++;
+        }
+    }
+    size = ht_size(set);
+    printf("Set size     : %d\n", size);
+    printf("Bucket amount: %d\n", maxhtlength);
+    printf("Load         : %d\n", load_factor);
+
+    data->first = last;
+    data->range = range;
+    data->update = update;
+    data->load_factor = load_factor;
+    data->move = move;
+    data->snapshot = snapshot;
+    data->unit_tx = unit_tx;
+    data->alternate = alternate;
+    data->effective = effective;
+    data->nb_add = 0;
+    data->nb_added = 0;
+    data->nb_remove = 0;
+    data->nb_removed = 0;
+    data->nb_move = 0;
+    data->nb_moved = 0;
+    data->nb_snapshot = 0;
+    data->nb_snapshoted = 0;
+    data->nb_contains = 0;
+    data->nb_found = 0;
+    data->nb_aborts = 0;
+    data->set = set;
+
+    printf("---------------------------Thread %d\n", RCCE_ue());
+    printf("  #add        : %lu\n", data->nb_add);
+    printf("    #added    : %lu\n", data->nb_added);
+    printf("  #remove     : %lu\n", data->nb_remove);
+    printf("    #removed  : %lu\n", data->nb_removed);
+    printf("  #contains   : %lu\n", data->nb_contains);
+    printf("    #found    : %lu\n", data->nb_found);
+    printf("  #move       : %lu\n", data->nb_move);
+    printf("  #moved      : %lu\n", data->nb_moved);
+    printf("  #snapshot   : %lu\n", data->nb_snapshot);
+    printf("  #snapshoted : %lu\n", data->nb_snapshoted);
+    FLUSH;
+
+    // Delete set 
+    ht_delete(set);
+
+    free(threads);
+    free(data);
+
+    return 0;
+}
