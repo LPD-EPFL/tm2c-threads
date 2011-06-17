6beb40d53fa4f621f00e51888ee15b5f46ffe2da
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-17 12:41:33 +0000

    30% write on up tx
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2524 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_tm7.c
+++ apps/iRCCE_tm7.c
@@ -14,7 +14,7 @@ stm_tx_node_t * stm_tx_node;
 #define SHMEM_SIZE      SIS_SIZE
 #define NUM_TXOPS       100
 #define UPDTX_PRCNT     20
-#define WRITE_PRCNT     20
+#define WRITE_PRCNT     30
 
 #define FOR(seconds)    double starting__ = RCCE_wtime(), duration__; while ((duration__ = (RCCE_wtime() - starting__)) < (seconds))
 #define ROLL(prcntg)    if (rand_range(100) <= (prcntg))
