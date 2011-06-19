9481b522f6d3f42ce38185f72fe9e4c7160e68ff
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-19 15:14:18 +0000

    using set->buckets[0] as an offset
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2546 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/hashtable/hashtable.h
+++ bmarks/microbench/hashtable/hashtable.h
@@ -32,6 +32,11 @@
 
 #define MAXHTLENGTH                     65536
 
+#ifdef SET
+#undef SET
+#define SET (set->buckets[0])
+#endif
+
 /* Hashtable length (# of buckets) */
 extern unsigned int maxhtlength;
 
