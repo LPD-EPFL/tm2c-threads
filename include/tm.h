68b6fbef0733ff025e158c423be65ee559eca09b
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-20 13:30:32 +0000

    testing with backoff delay 100
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2590 dd814760-9b98-ad33-1d6a-b699670a4318

--- include/tm.h
+++ include/tm.h
@@ -28,7 +28,7 @@ extern "C" {
 
 #define BACKOFF
 #define BACKOFF_MAX                     3
-#define BACKOFF_DELAY                   400
+#define BACKOFF_DELAY                   100
 
     extern stm_tx_t *stm_tx;
     extern stm_tx_node_t *stm_tx_node;
