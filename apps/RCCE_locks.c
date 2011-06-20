0a0059f4ee7970a6335a3b280f68c8bec46a28da
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-20 15:18:27 +0000

    testing
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2597 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/RCCE_locks.c
+++ apps/RCCE_locks.c
@@ -12,33 +12,36 @@
 unsigned int ID;
 #define ME printf("[%02d] ", RCCE_ue())
 #define P(args...) ME; printf(args); printf("\n"); fflush(stdout)
+#define BMS { double __duration, __stime; __stime = RCCE_wtime();
+#define BME __duration = RCCE_wtime() - __stime; P("Duration: %f", __duration);}
 
 int main(int argc, char** argv) {
     RCCE_init(&argc, &argv);
     ID = RCCE_ue();
-    
+
     P("starting");
 
-    P("RCCE_release_lock : explicitely");
-    RCCE_release_lock(ID);
-    P("RCCE_is_lock_free : %d", RCCE_is_lock_free(ID));
-    P("RCCE_is_lock_free : %d", RCCE_is_lock_free(ID));
-    
-    
-/*
-    P("RCCE_release_lock : explicitely");
-    //P("RCCE_test_lock : %d", RCCE_test_lock(ID));
-    P("RCCE_acquire_lock");
-    RCCE_acquire_lock(ID);
-    P("RCCE_test_lock : %d", RCCE_is_lock_free(ID));
-    P("RCCE_release_lock");
-    RCCE_release_lock(ID);
-    P("RCCE_test_lock : %d", RCCE_is_lock_free(ID));
-    RCCE_set_lock(ID);
-    P("RCCE_test_lock : %d", RCCE_is_lock_free(ID));
-    RCCE_set_lock(ID);
-    P("RCCE_test_lock : %d", RCCE_is_lock_free(ID));
-*/
+    int i;
+    for (i = 0; i < RCCE_num_ues(); i++) {
+        int is_free;
+        P("is_free %02d", i);
+        BMS
+        is_free = RCCE_is_lock_free(i);
+        BME
+        P("set %02d", i);
+        BMS
+        RCCE_set_lock(i);
+        BME
+        P("is_free %02d", i);
+        BMS
+        is_free = RCCE_is_lock_free(i);
+        BME
+        P("rls %02d", i);
+        BMS
+        is_free = RCCE_release_lock(i);
+        BME
+    }
+
 
 
     RCCE_finalize();
