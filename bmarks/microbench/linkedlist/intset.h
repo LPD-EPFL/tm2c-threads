d8a78fa9b68c6998549caae83a7b7de13e3a6c31
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-24 10:19:31 +0000

    dump
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2773 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/intset.h
+++ bmarks/microbench/linkedlist/intset.h
@@ -9,3 +9,7 @@
 
 #include "harris.h"
 
+int set_contains(intset_t *set, val_t val, int transactional);
+int set_add(intset_t *set, val_t val, int transactional);
+int set_remove(intset_t *set, val_t val, int transactional);
+
