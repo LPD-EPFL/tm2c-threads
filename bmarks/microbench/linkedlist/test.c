b4b0122c2ec16c01cb38e4944eb234cd943dcce9
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-06-30 16:49:37 +0000

    gathering changes
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2853 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/test.c
+++ bmarks/microbench/linkedlist/test.c
@@ -388,6 +388,25 @@ TASKMAIN(int argc, char **argv) {
     printf("---------------------------------------------------");
     FLUSH;
     /* Delete set */
+    
+    BARRIER
+    
+    int size_after;
+    ONCE {
+        size_after = set_size(set);
+    }
+    
+    BARRIER
+    
+    int * changes = (int *) ND(set->head);
+    int mychanges = data->nb_added - data->nb_removed;
+    PRINT("my changes :: \t\t%d", mychanges);
+    TX_START
+    int cc = *(int *) TX_LOAD(changes);
+    int newc = cc + mychanges;
+    TX_STORE(changes, &newc, TYPE_INT);
+    TX_COMMIT
+    PRINT("saved my changes");
 
 
 
