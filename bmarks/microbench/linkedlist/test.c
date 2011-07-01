d721a9ec4003e5be811d8cc4cde337a183f23036
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-07-01 08:49:21 +0000

    collecting data in the end
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2864 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/test.c
+++ bmarks/microbench/linkedlist/test.c
@@ -388,30 +388,41 @@ TASKMAIN(int argc, char **argv) {
     printf("---------------------------------------------------");
     FLUSH;
     /* Delete set */
-    
+
     BARRIER
-    
-    int size_after;
-    ONCE {
-        size_after = set_size(set);
-        set->head = 0;
-    }
-    
-    BARRIER
-    
-    int changes = set->head;
+
+            int *changes = (int *) set;
+    int *sequencer = changes + sizeof (int);
     int mychanges = data->nb_added - data->nb_removed;
+
+    int size_after;
+    ONCE
+    {
+        size_after = set_size(set);
+        *changes = 0;
+        *sequencer = 0;
+    }
+
+    BARRIER
+
     PRINT("my changes :: \t\t%d", mychanges);
-    //udelay(ID * 100);
+
     TX_START
-    int cc = *(int *) TX_LOAD(&changes);
+    if ((*(int *) TX_LOAD(sequencer)) != ID) {
+        udelay(ID * 50);
+        TX_ABORT(WRITE_AFTER_WRITE);
+    }
+    int id1 = ID + 1;
+    TX_STORE(sequencer, &id1, TYPE_INT);
+    int cc = *(int *) TX_LOAD(changes);
     int newc = cc + mychanges;
-    TX_STORE(&changes, &newc, TYPE_INT);
+    TX_STORE(changes, &newc, TYPE_INT);
     TX_COMMIT
-            
+
     BARRIER
     PRINT("saved my changes");
-    ONCE {
+    ONCE
+    {
         PRINT(":: ~~ :: Set size: %d, expected: %d", size_after, initial + set->head);
     }
 
