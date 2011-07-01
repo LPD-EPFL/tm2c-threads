d601346364c437c2cd04af9a5b9b2fb50e545473
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-07-01 15:14:11 +0000

    early rls
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@2899 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/linkedlist.h
+++ bmarks/microbench/linkedlist/linkedlist.h
@@ -26,8 +26,8 @@
  */
 #define SEQUENTIAL_
 #define STM
-#define EARLY_RELEASE_
-#define READ_VALIDATION
+#define EARLY_RELEASE
+#define READ_VALIDATION_
 
 #ifdef READ_VALIDATION
 #ifdef EARLY_RELEASE
