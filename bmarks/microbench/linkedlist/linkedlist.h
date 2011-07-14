847be81ada0fa58f81b909161015441308a45d50
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-07-14 16:24:39 +0000

    STM
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3008 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/linkedlist.h
+++ bmarks/microbench/linkedlist/linkedlist.h
@@ -24,8 +24,8 @@
  * SETTINGS
  * _____________________________________________________________________________
  */
-#define SEQUENTIAL
-#define STM_
+#define SEQUENTIAL_
+#define STM
 #define EARLY_RELEASE_
 #define READ_VALIDATION_
 
