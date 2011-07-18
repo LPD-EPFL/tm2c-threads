b48da5ff3e4f00082452f768ea946fb598aa8f3a
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-07-18 16:10:41 +0000

    removed SEQ flag
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3040 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/linkedlist.h
+++ bmarks/microbench/linkedlist/linkedlist.h
@@ -24,7 +24,7 @@
  * SETTINGS
  * _____________________________________________________________________________
  */
-#define SEQUENTIAL
+#define SEQUENTIAL_
 #define STM
 #define EARLY_RELEASE_
 #define READ_VALIDATION
