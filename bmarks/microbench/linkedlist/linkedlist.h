e92c35836770ee9ca2b20df3bb1500bcc8c72a11
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-10-03 19:16:37 +0000

    locks
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3290 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/linkedlist.h
+++ bmarks/microbench/linkedlist/linkedlist.h
@@ -25,7 +25,7 @@
  * _____________________________________________________________________________
  */
 #define SEQUENTIAL
-#define LOCKS_
+#define LOCKS
 #define STM_
 #define EARLY_RELEASE_
 #define READ_VALIDATION_
