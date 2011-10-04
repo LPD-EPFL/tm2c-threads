523db64a449eca33d78988a693965edaba85d74e
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-10-04 13:06:10 +0000

    testing the mem initialization
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3297 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/hashtable/test.c
+++ bmarks/microbench/hashtable/test.c
@@ -602,9 +602,9 @@ TASKMAIN(int argc, char **argv) {
     }
     int fo = ((off * 16) * 1024 * 1024) + ((ID/2) * 1024 * 1024) - (initial * sizeof (node_t));
     shmem_init(fo);
-    PRINT("shmem from %d MB", ((off * 16)));
+    PRINT("shmem from %d MB", fo);
     //shmem_init((RCCE_ue() * 1024 * 1024) - (initial * sizeof(node_t)));
-    
+    EXIT(0);
 #else
     shmem_init(1024*100*RCCE_ue()*sizeof(node_t) + (initial + 2)*sizeof(node_t));
 #endif
