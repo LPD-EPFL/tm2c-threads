ffd25983153fb6965ca927e896d47a60f2c584e5
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-10-04 13:24:56 +0000

    fixed mem initialization
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3301 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/test.c
+++ bmarks/microbench/linkedlist/test.c
@@ -376,35 +376,42 @@ TASKMAIN(int argc, char **argv) {
     }
 
 #ifdef STM
-    int off;
+        int off, id2use;
     if (ID < 6) {
         off = 0;
+        id2use = ID;
     }
     else if (ID < 12) {
         off = 1;
+        id2use = ID - 6;
     }
     else if (ID < 18) {
         off = 0;
+        id2use = ID - 6;
     }
     else if (ID < 24) {
         off = 1;
+        id2use = ID - 12;
     }
     else if (ID < 30) {
         off = 2;
+        id2use = ID - 24;
     }
     else if (ID < 36){
         off = 3;
+        id2use = ID - 30;
     }
     else if (ID < 42) {
         off = 2;
+        id2use = ID - 30;
     }
     else if (ID < 48) {
         off = 3;
+        id2use = ID - 36;
     }
-
-    shmem_init(((off * 16) * 1024 * 1024) + ((ID/2) * 40 * 1024) - (initial * sizeof (node_t)));
-    PRINT("shmem from %d MB, %d KB", ((off * 16)), (ID * 10));
-    //shmem_init((RCCE_ue() * 1024 * 1024) - (initial * sizeof(node_t)));
+    
+    shmem_init(((off * 16) * 1024 * 1024) + ((id2use/2) * 1024 * 1024));
+    PRINT("shmem from %d MB", (off * 16) + id2use/2);
 #else
     shmem_init(1024*100*RCCE_ue()*sizeof(node_t) + ((initial + 2) * sizeof(node_t)));
     
