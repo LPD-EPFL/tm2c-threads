c9022c926afdaed55bc3e5cbd8e4b2008ea7e8da
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-11-21 16:43:00 +0000

    debugging
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3552 dd814760-9b98-ad33-1d6a-b699670a4318

--- include/pgas.h
+++ include/pgas.h
@@ -18,7 +18,7 @@ extern "C" {
 
 #define SHMEM_SIZE      (1024 * 1024)           //1 KB
 #define SHMEM           shmem__
-//#define NUM_DSL_NODES   2
+    //#define NUM_DSL_NODES   2
 #define PGAS_TYPE       long long
 #define PGAS_GRAN       sizeof(long long int)
 
@@ -31,13 +31,19 @@ extern "C" {
     extern void PGAS_free(void *);
 
 #define ROUND(n)        (((int) n) == n ? (int) n : (int) n + 1)
-    
+
 #define PGAS_write(addr, val, type)             \
         *((type *) ((type *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES))) = (type) (val)
     //*((type) ((PGAS_TYPE) SHMEM + ((int) ((addr)/NUM_DSL_NODES)))) = (type) (val)
 
-#define PGAS_read(addr)                         \
-        ((int *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES))
+    //#define PGAS_read(addr)                         \
+//        ((int *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES))
+
+    inline int PGAS_read(unsigned int addr) {
+        PRINT("addr: %2d, rounded: %2d, pointer: %p", addr, ROUND((double) (addr) / NUM_DSL_NODES),
+                ((int *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES)));
+        return ((int *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES));
+    }
 
 
 
