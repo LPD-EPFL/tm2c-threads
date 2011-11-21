b26d2f3bcefd7153d177283de062cdbbbb6e1d5e
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-11-21 17:14:35 +0000

    pgas_read macro -> rets int
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3559 dd814760-9b98-ad33-1d6a-b699670a4318

--- include/pgas.h
+++ include/pgas.h
@@ -37,7 +37,7 @@ extern "C" {
     //*((type) ((PGAS_TYPE) SHMEM + ((int) ((addr)/NUM_DSL_NODES)))) = (type) (val)
 
     #define PGAS_read(addr)                         \
-        ((int *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES))
+        *((int *) SHMEM + ROUND((double) (addr) / NUM_DSL_NODES))
 
 //    inline int PGAS_read(unsigned int addr) {
 //        PRINT("addr: %2d, rounded: %2d, pointer: %p", addr, ROUND((double) (addr) / NUM_DSL_NODES),
