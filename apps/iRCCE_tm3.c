570a76aeabbeecdb6f8dd6752532e53fe6dfb9d0
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-11-21 15:54:29 +0000

    using tm3 to test pgas
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3545 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_tm3.c
+++ apps/iRCCE_tm3.c
@@ -1,5 +1,5 @@
 /*
- * Test if the write-set -> read-set work properly
+ * Testing PGAS
  */
 
 #include <assert.h>
@@ -15,47 +15,14 @@ MAIN(int argc, char **argv) {
 
     TM_INIT
 
-            int *sis = (int *) RCCE_shmalloc(SIS_SIZE * sizeof (int));
-    if (sis == NULL) {
-        PRINTD("RCCE_shmalloc");
-        EXIT(-1);
-    }
-
-    int i;
-    for (i = ID; i < SIS_SIZE; i += NUM_UES) {
-        sis[i] = -1;
-    }
-
-    BARRIER
-
-    //testing write set
     TX_START
-    TX_STORE(sis + ID, &ID, TYPE_INT);
-    ME; printf("sis[%d] = %d | ", ID, *(int *) TX_LOAD(&sis[ID]));
-    int doubleID = 2 * ID;
-    TX_STORE(sis + ID, &doubleID, TYPE_INT);
-    printf("sis[%d] = %d\n", ID, *(int *) TX_LOAD(&sis[ID]));
-    FLUSH
+
+            int i;
+    for (i = 0; i < 5; i++) {
+        PRINT("loading: %2d, value: %d", i, *(int *) TX_LOAD(i));
+    }
     TX_COMMIT
 
-    //testing read set
-    TX_START
-    sis[ID] == 665;
-    int tripleID = 3 * ID;
-    TX_STORE(sis + ID, &tripleID, TYPE_INT);
-    int temp = *(int *) TX_LOAD(&sis[ID]);
-    ME; printf("sis[%d] = %d | ", ID, temp);
-    assert(temp == 3 * ID);
-    sis[ID] = 666; //cheating TM
-    temp = *(int *) TX_LOAD(&sis[ID]);
-    printf("sis[%d] = %d\n", ID, temp);
-    FLUSH
-    TX_COMMIT
-    
-    PRINTD("finally : sis[%d] = %d\n", ID, sis[ID]);
-
-    RCCE_shfree((t_vcharp) sis);
-    
     TM_END
     EXIT(0);
 }
