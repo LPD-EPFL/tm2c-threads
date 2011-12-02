369ef7fb0daa92b204fdb69a310461f30447f6ec
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-12-02 18:18:39 +0000

    print debug
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3742 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlistpgas/linkedlist.c
+++ bmarks/microbench/linkedlistpgas/linkedlist.c
@@ -117,7 +117,7 @@ static int set_seq_add(intset_t *set, val_t val) {
     TX_START
     prev = (node_t) TX_LOAD(set->head);
     next = (node_t) TX_LOAD(prev.next);
-    PRINT("set->head: %d, next.next: %d", set->head, next.next);
+    PRINT("set->head: %d, next: %d, next.next", set->head, prev.next, next.next);
     while (next.val < val) {
         prev = next;
         next = (node_t) TX_LOAD(prev.next);
