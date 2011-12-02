2c2d1b81ce4426e83862c8a11d3059108a852206
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-12-02 18:34:49 +0000

    not allowing inf loop
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3752 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlistpgas/linkedlist.c
+++ bmarks/microbench/linkedlistpgas/linkedlist.c
@@ -115,10 +115,11 @@ static int set_seq_add(intset_t *set, val_t val) {
     node_t prev, next;
 
     TX_START
+    int i = 10;
     prev = (node_t) TX_LOAD(set->head);
     next = (node_t) TX_LOAD(prev.next);
     PRINT("set->head: %d, next: %d, next.next: %d", set->head, prev.next, next.next);
-    while (next.val < val) {
+    while (next.val < val && i--) {
         prev = next;
         next = (node_t) TX_LOAD(prev.next);
         PRINT("next : (%d)(%d)", next.val, next.next);
