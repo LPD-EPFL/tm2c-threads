f7d4f57639850394c86e12d2a5912e9e3567eb32
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-12-06 14:01:10 +0000

    others{} was in once{}
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3785 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlistpgas/test.c
+++ bmarks/microbench/linkedlistpgas/test.c
@@ -380,34 +380,33 @@ TASKMAIN(int argc, char **argv) {
         assert(size == initial);
         FLUSH
 
+    }
+    OTHERS
+    {
+        PRINT("----------------------------------------------------------------");
+        PRINT("----------------------------------------------------------------");
+        PRINT("----------------------------------------------------------------");
+        PRINT("Adding: %d", 666);
+        set_add(set, 666, 1);
+        PRINT("Adding: %d", 55666);
+        set_add(set, 55666, 1);
+        set_print(set);
+        int c = 666;
+        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
+        c = 5666;
+        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
+        c = 55666;
+        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
+        c = 1;
+        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
+        c = 65000;
+        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
 
-        OTHERS {
-                        PRINT("----------------------------------------------------------------");
-                        PRINT("----------------------------------------------------------------");
-                        PRINT("----------------------------------------------------------------");
-                        PRINT("Adding: %d", 666);
-                        set_add(set, 666, 1);
-                        PRINT("Adding: %d", 55666);
-                        set_add(set, 55666, 1);
-                        set_print(set);
-                        int c = 666;
-                        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
-                        c = 5666;
-                        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
-                        c = 55666;
-                        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
-                        c = 1;
-                        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
-                        c = 65000;
-                        PRINT("set contains %d = %d", c, set_contains(set, c, 1));
-        
-                        PRINT("Removing: %d", 666);
-                        set_remove(set, 666, 1);
-                        PRINT("Removing: %d", 55666);
-                        set_remove(set, 55666, 1);
-                        set_print(set);
-        }
-
+        PRINT("Removing: %d", 666);
+        set_remove(set, 666, 1);
+        PRINT("Removing: %d", 55666);
+        set_remove(set, 55666, 1);
+        set_print(set);
     }
 
     /* Access set from all threads */
