f78d5743e10c956cd3bd198ea3a40d31d9928c56
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-12-07 16:42:50 +0000

    adding duplicate
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3851 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/hashtablepgas/test.c
+++ bmarks/microbench/hashtablepgas/test.c
@@ -544,6 +544,7 @@ TASKMAIN(int argc, char **argv) {
         print_ht(set);
         ht_add(set, 666, 1);
         ht_add(set, 55555, 1);
+        ht_add(set, 666, 1);
         print_ht(set);
         ht_delete(set, 55555);
         ht_delete(set, 666);
