a7cf5e67f7c134124958c007916e5ee132e98503
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-12-19 18:24:37 +0000

    less pring
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3897 dd814760-9b98-ad33-1d6a-b699670a4318

--- src/pubSubTM.c
+++ src/pubSubTM.c
@@ -162,7 +162,6 @@ CONFLICT_TYPE ps_subscribe(void *address) {
 
 #ifdef PGAS
     //ps_send_rl(responsible_node, (unsigned int) address);
-    PRINT("\t\t\t\tAddress: %2d -> %d | NUM_DSL_NODES: %d", address, SHRINK(address), NUM_DSL_NODES);
     ps_send_rl(responsible_node, SHRINK(address));
 #else
     ps_sendb(responsible_node, PS_SUBSCRIBE, address_offs, NO_CONFLICT);
