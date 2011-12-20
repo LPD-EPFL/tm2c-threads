a3a471f7e4a4eb895f26d94d407166db5033a296
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2011-12-20 16:06:03 +0000

    testing the bug with deddsl version
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@3931 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_tm3.c
+++ apps/iRCCE_tm3.c
@@ -7,32 +7,55 @@
 #include "tm.h"
 
 #define SIS_SIZE 480
+#define REPS 31
 
 stm_tx_t *stm_tx;
 stm_tx_node_t *stm_tx_node;
 
 MAIN(int argc, char **argv) {
 
-    int steps = 0;
+    int steps = REPS;
+
 
     TM_INIT
 
-
-    TX_START
-    int i;
-    for (i = 0; i < 10; i++) {
-        TX_LOAD_STORE(i, +, i);
+    if (argc >= 1) {
+        steps = atoi(argv[1]);
     }
-    
-    TX_COMMIT
-    TX_START
-    int i;
-    for (i = 0; i < 10; i++) {
+
+    /*
+        int once = 1;
+        TX_START
+        PRINT("will i abort? %d", once);
+        TX_LOAD_STORE(1, +, 1);
+        if (once--) {
+            TX_ABORT(NO_CONFLICT);
+        }
+        TX_COMMIT
+     */
+
+    ONCE
+    {
+        TX_START
+                int i;
+        for (i = 0; i < steps; i++) {
+            TX_LOAD_STORE(i, +, 1);
+        }
+
+        TX_COMMIT
+    }
+    BARRIER;
+
+    ONCE
+    {
+        TX_START
+                int i, k = (int) ((double) steps / 10) + 1;
+        for (i = 0; i < steps; i += k) {
             PRINT("address %2d, value %2d", i, TX_LOAD(i));
-    }
-    
-    TX_COMMIT
+        }
 
+        TX_COMMIT
+    }
     TM_END
     EXIT(0);
 }
