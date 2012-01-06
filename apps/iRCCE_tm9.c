533bdbbefe956968e53a8430da9f69cca68de954
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-01-06 11:30:12 +0000

    variable reps
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4008 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_tm9.c
+++ apps/iRCCE_tm9.c
@@ -4,7 +4,7 @@
 
 #include "tm.h"
 
-#define SIS_SIZE 48
+#define SIS_SIZE 100
 
 stm_tx_t *stm_tx;
 stm_tx_node_t *stm_tx_node;
@@ -13,8 +13,17 @@ stm_tx_node_t *stm_tx_node;
 MAIN(int argc, char **argv) {    
     TM_INIT
     
+    int reps = SIS_SIZE;
     
-    int *j = (int *) RCCE_shmalloc(1000);
+    if (argc > 1) {
+        reps = atoi(argv[1]);
+    }
+    
+    ONCE {
+        PRINT("Repetitions %d", reps);
+    }
+    
+    int *j = (int *) RCCE_shmalloc(reps);
 
     BARRIER
     
@@ -26,7 +35,7 @@ MAIN(int argc, char **argv) {
     TX_START
     
     int i;
-    for (i = 0; i < 1000; i++) {
+    for (i = 0; i < reps; i++) {
         //PRINT("loading %2d, %p", i, j+i);
         TX_LOAD(j + i);
     }
