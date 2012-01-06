305ed6f8f510649fc821acd8f139690d290ce04f
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-01-06 15:52:39 +0000

    more test for PF
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4030 dd814760-9b98-ad33-1d6a-b699670a4318

--- apps/iRCCE_tm1.c
+++ apps/iRCCE_tm1.c
@@ -16,15 +16,45 @@ MAIN(int argc, char **argv) {
 
     TM_INIT
 
+    short j = 0; 
     long long int ll = 10000000;
-    
+
     while (ll--) {
-        PF_START(0);
-        PF_STOP(0);
+        PF_START(j);
+        PF_STOP(j);
+    }
+
+    j++;
+    ll = 1000000;
+    while (ll--) {
+        PF_START(j);
+        PF_STOP(j);
+    }
+
+    j++;
+    ll = 100000;
+    while (ll--) {
+        PF_START(j);
+        PF_STOP(j);
     }
     
+    j++;
+    ll = 10000;
+    while (ll--) {
+        PF_START(j);
+        PF_STOP(j);
+    }
+
+    j++;
+    ll = 1000;
+    while (ll--) {
+        PF_START(j);
+        PF_STOP(j);
+    }
+
+
     PF_PRINT
-    
+
 
     TM_END
     EXIT(0);
