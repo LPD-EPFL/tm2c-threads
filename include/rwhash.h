e77e4c814b0f7561460eb72c16d75fbff9e513b7
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-01-06 11:27:18 +0000

    num_of_bucks 3
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4004 dd814760-9b98-ad33-1d6a-b699670a4318

--- include/rwhash.h
+++ include/rwhash.h
@@ -35,7 +35,7 @@ extern "C" {
 
 #define NO_WRITERI      0x10000000
 #define NO_WRITER       0x1000
-#define NUM_OF_BUCKETS  16
+#define NUM_OF_BUCKETS  3
 
 //#define HASH(address)   ((((((unsigned int) address)) ^ 2) >> 8))
 //#define HASH(address)   ((((((unsigned int) address)))>> 3))
