99c6e6e0b690912d6f3d08a90d066654d999c029
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-01-19 10:32:43 +0000

    no actual subscribe
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4220 dd814760-9b98-ad33-1d6a-b699670a4318

--- src/dslock.c
+++ src/dslock.c
@@ -149,8 +149,8 @@ static void dsl_communication() {
                     //PRINT("RL addr: %3d, val: %d", ps_remote->address, PGAS_read(ps_remote->address));
                     ps_send(sender, PS_SUBSCRIBE_RESPONSE, PGAS_read(ps_remote->address), try_subscribe(sender, ps_remote->address));
 #else
-                    ps_send(sender, PS_SUBSCRIBE_RESPONSE, ps_remote->address, try_subscribe(sender, ps_remote->address));
-                    //ps_send(sender, PS_SUBSCRIBE_RESPONSE, ps_remote->address, NO_CONFLICT);
+                    //ps_send(sender, PS_SUBSCRIBE_RESPONSE, ps_remote->address, try_subscribe(sender, ps_remote->address));
+                    ps_send(sender, PS_SUBSCRIBE_RESPONSE, ps_remote->address, NO_CONFLICT);
 #endif
                     //ps_send(sender, PS_SUBSCRIBE_RESPONSE, ps_remote->address, NO_CONFLICT);
                     break;
