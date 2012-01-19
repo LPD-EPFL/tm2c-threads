3e7f0f6c003b7aeebb155a890bb5c319ab89a99c
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-01-19 19:44:47 +0000

    3 50
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4275 dd814760-9b98-ad33-1d6a-b699670a4318

--- include/tm.h
+++ include/tm.h
@@ -37,8 +37,8 @@ extern "C" {
 #define BACKOFF_MAX                     3
 #define BACKOFF_DELAY                   50
 #else
-#define BACKOFF_MAX                     5
-#define BACKOFF_DELAY                   200
+#define BACKOFF_MAX                     3
+#define BACKOFF_DELAY                   50
 #endif
 
 
