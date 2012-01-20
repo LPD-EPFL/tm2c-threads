78ad0945f09c85d461f20e3014c573369dda6da0
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-01-20 13:20:58 +0000

    transactional
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4291 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlist/linkedlist.h
+++ bmarks/microbench/linkedlist/linkedlist.h
@@ -24,7 +24,7 @@
  * SETTINGS
  * _____________________________________________________________________________
  */
-#define SEQUENTIAL
+#define SEQUENTIAL_
 #define LOCKS_
 #define STM
 #define EARLY_RELEASE_
