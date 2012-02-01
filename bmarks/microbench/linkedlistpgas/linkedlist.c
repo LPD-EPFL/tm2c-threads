007edcf251cd337de0a14933afd0f7f5040e0971
v.trigonakis@gmail.com <v.trigonakis@gmail.com@dd814760-9b98-ad33-1d6a-b699670a4318>
2012-02-01 13:25:52 +0000

    print debug
    git-svn-id: https://trigonak-thesis.googlecode.com/svn/exjobb/iRCCEdeddsl@4396 dd814760-9b98-ad33-1d6a-b699670a4318

--- bmarks/microbench/linkedlistpgas/linkedlist.c
+++ bmarks/microbench/linkedlistpgas/linkedlist.c
@@ -123,6 +123,7 @@ static int set_seq_add(intset_t *set, val_t val) {
     pgas_addr_t prev_addr;
 
     TX_START
+    PRINT("adding %d", val);
     prev = (node_t) TX_LOAD(set->head);
     prev_addr = set->head;
     next = (node_t) TX_LOAD(prev.next);
