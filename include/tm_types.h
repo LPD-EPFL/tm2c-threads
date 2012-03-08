bfdeef9a799273299401b915985ca0eb93747b7f
Nikola Knezevic <laladelausanne@gmail.com>
2012-03-08 22:19:39 +0100

    Add printf formats for tm_addr_t and tm_intern_addr

--- include/tm_types.h
+++ include/tm_types.h
@@ -4,6 +4,14 @@
 #include <inttypes.h>
 #include <stdint.h>
 
+#if SIZE_MAX == 4294967295
+#define PRIxSIZE PRIx32
+#elif SIZE_MAX > 4294967295
+#define PRIxSIZE PRIx64
+#else
+#define PRIxSIZE x
+#endif
+
 typedef enum {
 	FALSE, //0
 	TRUE //1
@@ -12,16 +20,27 @@ typedef enum {
 typedef unsigned int nodeid_t;
 typedef void* tm_addr_t;
 
+/*
+ * The format in printf to represent tm_addr_t
+ */
+#define PRIxA "p"
+
 #ifdef PGAS
 /*
  * With PGAS, addresses that are handled by the dslock nodes are offsets
  */
 typedef size_t tm_intern_addr_t;
+
+/*
+ * The format in printf to represent this address
+ */
+#define PRIxIA PRIxSIZE 
 #else
 /*
  * However, for the non-PGAS code, they are uintptr_t 
  */
 typedef uintptr_t tm_intern_addr_t;
+#define PRIxIA PRIxPTR
 #endif
 
 /*
