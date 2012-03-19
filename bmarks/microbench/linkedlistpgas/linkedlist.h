6b2f9ae174b53c264e401e96c97dee0d992a50bb
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2012-03-19 15:19:08 +0100

    make the pgas/linkedlist.c same as the normal linkedlist
    the new pgas implementation will be more close to the normal tx than the old pgas

--- bmarks/microbench/linkedlistpgas/linkedlist.h
+++ bmarks/microbench/linkedlistpgas/linkedlist.h
@@ -36,12 +36,9 @@
 #endif
 #endif
 
-#define VAL_MIN                         0
-#define VAL_MAX                         USHRT_MAX
-
 #define DEFAULT_DURATION                10
 #define DEFAULT_INITIAL                 256
-#define DEFAULT_RANGE                   (VAL_MAX-2)
+#define DEFAULT_RANGE                   0x7FFFFFFF
 #define DEFAULT_UPDATE                  20
 #define DEFAULT_ELASTICITY		4
 #define DEFAULT_ALTERNATE		0
@@ -53,35 +50,41 @@
 
 #define TRANSACTIONAL                   d->unit_tx
 
-typedef unsigned short int val_t;
-typedef unsigned short int nxt_t;
+typedef intptr_t val_t;
+typedef unsigned int nxt_t;
 
-typedef union {
-    int toint;
-    struct {
-        val_t val;
+#define VAL_MIN                         INT_MIN
+#define VAL_MAX                         INT_MAX
+
+typedef struct node {
+    val_t val;
+
+    union {
+        struct node *nextp;
         nxt_t next;
     };
 } node_t;
 
 typedef struct intset {
-    nxt_t head;
+
+    union {
+        nxt_t head;
+        node_t *headp;
+    };
+
 } intset_t;
 
-typedef struct {
-    pgas_addr_t addr;
-    node_t node;
-} new_node_t;
+extern nxt_t offs__;
 
 #define N2O(set, node)                  (nxt_t) ((nxt_t) (node) - (nxt_t) (set))
-#define O2N(set, offset)                ((void *) (offset) == NULL ? NULL : (node_t *) ((nxt_t) (set) + (offset)))
+#define O2N(set, offset)                ((void *) (offs__ = offset) == NULL ? NULL : (node_t *) ((nxt_t) (set) + (offs__)))
 #define SET                             set
 #define ND(offs)                        O2N(SET, (offs))
 #define OF(node)                        N2O(SET, (node))
 
 void *shmem_init(size_t offset);
 
-new_node_t new_node(val_t val, nxt_t next, int transactional);
+node_t *new_node(val_t val, nxt_t next, int transactional);
 intset_t *set_new();
 void set_delete(intset_t *set);
 int set_size(intset_t *set);
@@ -93,13 +96,13 @@ int set_remove(intset_t *set, val_t val, int transactional);
 
 void set_print(intset_t *set);
 
-inline void global_lock() {
+INLINED void global_lock() {
     PRINTD("askking for global lock");
     RCCE_acquire_lock(0);
     PRINTD("got global lock");
 }
 
-inline void global_lock_release() {
+INLINED void global_lock_release() {
     RCCE_release_lock(0);
     PRINTD("released global lock");
 }
