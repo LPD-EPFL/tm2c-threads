32b9ad798704e116ed61df525cfe95e7e7592004
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2012-05-18 20:00:53 +0200

    added the linux kernel hash implementation

--- include/hash.h
+++ include/hash.h
@@ -16,6 +16,39 @@ hash_tw(unsigned int key)
   return key;
 }
 
+/* Fast hashing routine for ints,  longs and pointers.
+   (C) 2002 William Lee Irwin III, IBM */
+/*
+ * Knuth recommends primes in approximately golden ratio to the maximum
+ * integer representable by a machine word for multiplicative hashing.
+ * Chuck Lever verified the effectiveness of this technique:
+ * http://www.citi.umich.edu/techreports/reports/citi-tr-00-1.pdf
+ *
+ * These primes are chosen to be bit-sparse, that is operations on
+ * them can use shifts and additions instead of multiplications for
+ * machines where multiplications are slow.
+ */
 
+/* 2^31 + 2^29 - 2^25 + 2^22 - 2^19 - 2^16 + 1 */
+#define GOLDEN_RATIO_PRIME_32 0x9e370001UL
+/*  2^63 + 2^61 - 2^57 + 2^54 - 2^51 - 2^18 + 1 */
+#define GOLDEN_RATIO_PRIME_64 0x9e37fffffffc0001UL
+
+#define GOLDEN_RATIO_PRIME GOLDEN_RATIO_PRIME_32
+#define hash_long(val, bits) hash_32(val, bits)
+
+static inline unsigned int hash_32(unsigned int val, unsigned int bits)
+{
+  /* On some cpus multiply is faster, on others gcc will do shifts */
+  unsigned int hash = val * GOLDEN_RATIO_PRIME_32;
+
+  /* High bits are more random, so use them. */
+  return hash >> (32 - bits);
+}
+
+static inline unsigned long hash_ptr(const void *ptr, unsigned int bits)
+{
+  return hash_long((unsigned long)ptr, bits);
+}
 
 #endif /* _HASH_H */
