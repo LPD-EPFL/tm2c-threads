dc77b43e3965ad8d90c9f43e29ddf2eeb7130105
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2012-10-23 19:31:28 +0200

    added app_id_seq() function and formatted the code

--- include/sys_MCORE_ssmp.h
+++ include/sys_MCORE_ssmp.h
@@ -62,7 +62,9 @@ sys_sendcmd_all(void* data, size_t len)
   return 1;
 }
 
-static const uint32_t wait_after_send[4] = {880, 880, 880, 880};
+/* static const uint32_t wait_after_send[4] = {880, 880, 880, 880}; */
+#define vv 880
+static const uint32_t wait_after_send[4] = {vv, vv, vv, vv};
 
 INLINED int
 sys_recvcmd(void* data, size_t len, nodeid_t from)
