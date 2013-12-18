9b4300d83083b304285262cd1f16a106d4cca537
David Cervini <cervini@maglite.(none)>
2013-12-18 15:52:50 +0100

    niagara measures

--- experiment_bank.sh
+++ experiment_bank.sh
@@ -5,12 +5,10 @@ do
 	for core in 6 12 18 24 30 36 48
 	do
 			echo $command $core
-			$command -total\=$core \-R1 \-c0 > tmp
+			$command -total=$core -R1 -c0 > tmp
 			if [ $? -eq 0 ] 
 			then
-				awk -v c=$command -v co=$core \
-					'/Throughput/ {print c,co, $2}'\
-					tmp >> measures 
+				gawk -v c=$command -v co=$core '/Throughput/ {print c,co, $2}' tmp >> measures 
 			fi
 	done
 done
