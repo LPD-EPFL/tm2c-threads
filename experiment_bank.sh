c45a5170282fac693ef10a3008857de9c90c0bdb
David Cervini <cervini@lpd48core.epfl.ch>
2013-12-24 13:24:21 +0100

    measures

--- experiment_bank.sh
+++ experiment_bank.sh
@@ -5,7 +5,7 @@ do
 	for core in 6 12 18 24 30 36 48
 	do
 			echo $command $core
-			$command -total=$core -R1 -c0 > tmp
+			$command -total=$core -c90 > tmp
 			if [ $? -eq 0 ] 
 			then
 				gawk -v c=$command -v co=$core '/Throughput/ {print c,co, $2}' tmp >> measures 
