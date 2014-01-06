37e7b86db16e7002f29d0ad7fdfdc0cf9982e72e
David Cervini <cervini@lpd48core.epfl.ch>
2014-01-06 22:02:07 +0100

    expermit-bank

--- experiment_bank.sh
+++ experiment_bank.sh
@@ -4,12 +4,15 @@ for command in "./bmarks/bank"
 do
 	for core in 6 12 18 24 30 36 48
 	do
-			echo $command $core
-			$command -total=$core -c90 > tmp
-			if [ $? -eq 0 ] 
-			then
-				gawk -v c=$command -v co=$core '/Throughput/ {print c,co, $2}' tmp >> measures 
-			fi
+		for c in 0 50 90 
+		do
+				echo $command $core
+				$command -total=$core -c$c > tmp
+				if [ $? -eq 0 ] 
+				then
+					gawk -v command=$command -v core=$core -v c=$c '/Throughput/ {print command,"-total=" core,"-c" c, $2}' tmp >> measures 
+				fi
+		done
 	done
 done
 
