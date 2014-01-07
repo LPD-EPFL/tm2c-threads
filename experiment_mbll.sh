78e191fcb83e0b1d0f34aa2b33fbf42f9c78decd
David Cervini <cervini@lpd48core.epfl.ch>
2014-01-07 01:39:30 +0100

    new data

--- experiment_mbll.sh
+++ experiment_mbll.sh
@@ -2,20 +2,28 @@
 
 for command in "./bmarks/mbll" 
 do
-	for u in 0 10
+	for u in '-u0' '-u10'
 	do
-		for core in 2 3 4 6 12 18 24 30 36 48
+	for i in '-i1024 -r2048' '-i128 -r256'
+	do
+		for core in 6 12 18 24 30 36 48
 		do
-			echo $command $core
-			$command -i1024 -r2048 -u$u -total\=$core  > tmp
+			echo $command $i $u -total\=$core 
+			$command $i $u -total\=$core > tmp
 			if [ $? -eq 0 ] 
 			then
-				awk -v c=$command -v co=$core -v uu=$u \
-					'/Throughput/ {print c,co, "-i1024 -r2048 -u", uu, $2}'\
-					tmp >> measures 
+				ii=${i//"-"/""}
+				ii=${i//" "/""}
+				uu=${u//"-"/""}
+				echo $ii$uu
+				awk -v c=$command -v core=$core -v i=$ii -v u=$uu\
+					'/Throughput/ {print c,"-total=",core, i, u, $2}' tmp >> measures 
+				#awk -v c=$command -v core=$core -v i=$ii -v u=$uu \
+				#	'/Throughput/ {print c,"-total=",core, i, u, $2}' tmp >> measures 
 			fi
 		rm tmp
 		done
+	done
 	done
 done
 
